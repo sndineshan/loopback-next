dfc8a0745ceaa5189b4cfe0e4298defa264a3e48
Raymond Feng <rfeng@us.ibm.com>
2017-10-20 15:58:16 -0700

    feat(context): Add binding options and @inject.options
    1. Add options property and withOptions method for a binding
    2. Pass binding to dependency resolution
    3. Add @injection.options to receive injection of options
    4. Fix routing table to use dependency injection

--- packages/context/src/resolver.ts
+++ packages/context/src/resolver.ts
@@ -4,7 +4,7 @@
 // License text available at https://opensource.org/licenses/MIT
 
 import {Context} from './context';
-import {BoundValue, ValueOrPromise} from './binding';
+import {Binding, BoundValue, ValueOrPromise} from './binding';
 import {isPromise} from './is-promise';
 import {
   describeInjectedArguments,
@@ -28,13 +28,15 @@ export type Constructor<T> =
  *
  * @param ctor The class constructor to call.
  * @param ctx The context containing values for `@inject` resolution
+ * @param binding The optional binding of the class if bound
  */
 export function instantiateClass<T>(
   ctor: Constructor<T>,
   ctx: Context,
+  binding?: Binding,
 ): T | Promise<T> {
-  const argsOrPromise = resolveInjectedArguments(ctor, ctx);
-  const propertiesOrPromise = resolveInjectedProperties(ctor, ctx);
+  const argsOrPromise = resolveInjectedArguments(ctor, ctx, binding);
+  const propertiesOrPromise = resolveInjectedProperties(ctor, ctx, binding);
   let inst: T | Promise<T>;
   if (isPromise(argsOrPromise)) {
     // Instantiate the class asynchronously
@@ -46,19 +48,19 @@ export function instantiateClass<T>(
   if (isPromise(propertiesOrPromise)) {
     return propertiesOrPromise.then(props => {
       if (isPromise(inst)) {
-        // Inject the properties asynchrounously
+        // Inject the properties asynchronously
         return inst.then(obj => Object.assign(obj, props));
       } else {
-        // Inject the properties synchrounously
+        // Inject the properties synchronously
         return Object.assign(inst, props);
       }
     });
   } else {
     if (isPromise(inst)) {
-      // Inject the properties asynchrounously
+      // Inject the properties asynchronously
       return inst.then(obj => Object.assign(obj, propertiesOrPromise));
     } else {
-      // Inject the properties synchrounously
+      // Inject the properties synchronously
       return Object.assign(inst, propertiesOrPromise);
     }
   }
@@ -88,10 +90,12 @@ function resolve<T>(ctx: Context, injection: Injection): ValueOrPromise<T> {
  *
  * @param fn The function for which the arguments should be resolved.
  * @param ctx The context containing values for `@inject` resolution
+ * @param binding The optional binding of the class if bound
  */
 export function resolveInjectedArguments(
   fn: Function,
   ctx: Context,
+  binding?: Binding,
 ): BoundValue[] | Promise<BoundValue[]> {
   // NOTE: the array may be sparse, i.e.
   //   Object.keys(injectedArgs).length !== injectedArgs.length
@@ -103,13 +107,15 @@ export function resolveInjectedArguments(
   let asyncResolvers: Promise<void>[] | undefined = undefined;
 
   for (let ix = 0; ix < fn.length; ix++) {
-    const injection = injectedArgs[ix];
+    let injection = injectedArgs[ix];
     if (!injection.bindingKey && !injection.resolve) {
       throw new Error(
         `Cannot resolve injected arguments for function ${fn.name}: ` +
           `The argument ${ix + 1} was not decorated for dependency injection.`,
       );
     }
+    // Copy the injection so that we can set the parent binding
+    injection = Object.assign({binding}, injection);
 
     const valueOrPromise = resolve(ctx, injection);
     if (isPromise(valueOrPromise)) {
@@ -131,9 +137,22 @@ export function resolveInjectedArguments(
 
 export type KV = {[p: string]: BoundValue};
 
+/**
+ * Given a class with properties decorated with `@inject`,
+ * return the map of properties resolved using the values
+ * bound in `ctx`.
+
+ * The function returns an argument array when all dependencies were
+ * resolved synchronously, or a Promise otherwise.
+ *
+ * @param fn The function for which the arguments should be resolved.
+ * @param ctx The context containing values for `@inject` resolution
+ * @param binding The optional binding of the class if bound
+ */
 export function resolveInjectedProperties(
   fn: Function,
   ctx: Context,
+  binding?: Binding,
 ): KV | Promise<KV> {
   const injectedProperties = describeInjectedProperties(fn.prototype);
 
@@ -144,13 +163,15 @@ export function resolveInjectedProperties(
     (properties[p] = v);
 
   for (const p in injectedProperties) {
-    const injection = injectedProperties[p];
+    let injection = injectedProperties[p];
     if (!injection.bindingKey && !injection.resolve) {
       throw new Error(
         `Cannot resolve injected property for class ${fn.name}: ` +
           `The property ${p} was not decorated for dependency injection.`,
       );
     }
+    // Copy the injection so that we can set the parent binding
+    injection = Object.assign({binding}, injection);
     const valueOrPromise = resolve(ctx, injection);
     if (isPromise(valueOrPromise)) {
       if (!asyncResolvers) asyncResolvers = [];
